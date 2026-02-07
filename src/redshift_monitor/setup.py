e351356267dd9844cc3e51ae828508643526748f
Bat-Chen <bzeliger@gmail.com>
2026-02-07 11:34:43 -0500

    cleanup unused node

--- src/redshift_monitor/setup.py
+++ src/redshift_monitor/setup.py
@@ -20,7 +20,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'redshift_monitor = redshift_monitor.redshift_monitor:main',
             'redshift_lifesigns = redshift_monitor.redshift_lifesigns:main',            
         ],
     },
