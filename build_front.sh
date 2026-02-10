936970e1ae8f01da68d1359360b252fee102a3e3
Bat-Chen <bzeliger@gmail.com>
2026-02-10 07:24:45 -0500

    more changes for 2 images

--- build_front.sh
+++ build_front.sh
@@ -7,14 +7,14 @@ mkdir -p contents/ros2_ws
 mkdir -p contents/ros2_ws/log
 mkdir -p contents/ros2_ws/build
 mkdir -p contents/ros2_ws/install
-mkdir -p contents/ros2_ws/config
+mkdir -p contents/ros2_ws/redshift_odometry/config
 
 cp -r misc contents/ros2_ws
 cp -r src contents/ros2_ws
 cp install/* contents/ros2_ws/install
 
 # select FRONT RPi camera configuration
-cp misc/camera/front_rpi_camera.yaml contents/ros2_ws/config/camtable.yaml
+cp misc/camera/front_rpi_camera.yaml contents/ros2_ws/redshift_odometry/config/camtable.yaml
 
 cp redshift_entrypoint.sh contents
 cp start-*.sh contents
