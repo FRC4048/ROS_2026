3452636bb067b8b438978a88e83ffce3816312c7
Bat-Chen <bzeliger@gmail.com>
2026-02-25 22:36:29 -0500

    lifesigns now reads vision_constant from NT

--- src/redshift_monitor/redshift_monitor/redshift_lifesigns.py
+++ src/redshift_monitor/redshift_monitor/redshift_lifesigns.py
@@ -5,10 +5,6 @@ This module contains the Redshift Lifesigns ROS2 node, which acts as a heartbeat
 monitor for the robot system. It increments a counter and broadcasts it to 
 ROS2 topics and/or NetworkTables.
 
-Environment Variables:
-    PUB_ROS (str): Set to '0' or 'false' to disable ROS2 publishing. Defaults to True.
-    PUB_NT (str): Set to '1' or 'true' to enable NetworkTables publishing. Defaults to False.
-
 To start node:
     on rpi - started automatically from docker-compose.yaml
     testing - from terminal got to ros2_ws :
@@ -26,8 +22,7 @@ To start node:
 import rclpy
 import os
 from rclpy.node import Node
-from std_msgs.msg import UInt16
-from rcl_interfaces.msg import ParameterDescriptor
+from std_msgs.msg import UInt16, Float64
 import ntcore
 
 from roborio_msgs.msg import RoborioTags
@@ -42,8 +37,6 @@ class RedshiftLifesigns(Node):
     it to the parameter server for other nodes to use.
 
     Attributes:
-        ros_publish (bool): Whether the node is currently publishing to ROS.
-        nt_publish (bool): Whether the node is currently publishing to NetworkTables.
         publisher (rclpy.publisher.Publisher): The ROS2 publisher for heartbeat data.
         lifesigns_pub (ntcore.DoublePublisher): The NetworkTable publisher for heartbeat data.
         vision_constant_sub (ntcore.DoubleSubscriber): The NetworkTable subscriber for vision constant.
@@ -58,18 +51,6 @@ class RedshiftLifesigns(Node):
 
     def __init__(self):
         super().__init__('lifesigns')
-        
-        # Declare vision_constant parameter
-        self.declare_parameter(
-            'vision_constant', 
-            1.0/148.0,
-            ParameterDescriptor(
-                name='vision_constant',
-                type='double',
-                description='Vision constant from NetworkTables for odometry calculations'
-            )
-        )
-        
         self.ros_publish = True
         tmp = os.environ.get('PUB_ROS')
         if (tmp in ('0', 'false', 'False', 'f', 'F')):
@@ -77,30 +58,22 @@ class RedshiftLifesigns(Node):
         if (self.ros_publish):
            self.get_logger().info('Publishing to ROS')
 
-        self.nt_publish = False
-        tmp = os.environ.get('PUB_NT')
-        if (tmp in ('1', 'true', 'True', 'TRUE', 't', 'T')):
-           self.nt_publish = True
-        if (self.nt_publish):
-           self.get_logger().info("Publishing to NETWORK TABLES")
-
         # CREATE NETWORK TABLE CONNECTION AND PUBLISHER
-        if (self.nt_publish):
-            self.inst = ntcore.NetworkTableInstance.getDefault()
-            self.inst.startClient4("ROS Client")
-            self.inst.setServerTeam(4048)
-            #self.inst.setServer("192.168.1.160")
-            while not self.inst.isConnected():
-                pass
-            self.get_logger().info("Connected to NETWORK TABLES")
-            self.table = self.inst.getTable("ROS")
-            self.inst.startDSClient()
-            self.lifesigns_pub = self.table.getDoubleTopic("lifesigns").publish()
-            # Add vision constant subscriber
-            self.vision_constant_sub = self.table.getDoubleTopic("vision_constant").subscribe(1.0/148.0)
+        self.inst = ntcore.NetworkTableInstance.getDefault()
+        self.inst.startClient4("ROS Client")
+        self.inst.setServerTeam(4048)
+        #self.inst.setServer("192.168.2.191")
+        while not self.inst.isConnected():
+            pass
+        self.get_logger().info("Connected to NETWORK TABLES")
+        self.table = self.inst.getTable("ROS")
+        self.inst.startDSClient()
+        self.lifesigns_pub = self.table.getDoubleTopic("lifesigns").publish()
+        # Add vision constant subscriber
+        self.vision_constant_sub = self.table.getDoubleTopic("vision_constant").subscribe(1.0/148.0)
         
-        # CREATE ROS2 PUBLISHER
-        self.publisher = self.create_publisher(UInt16, "/redshift/lifesigns", 10)
+        # CREATE vision_constant PUBLISHER
+        self.vision_constant_publisher = self.create_publisher(Float64, "/redshift/vision_constant", 10)
         
         # INITIALIZE COUNTERS AND VALUES
         self.lifesigns_counter = UInt16()
@@ -112,25 +85,22 @@ class RedshiftLifesigns(Node):
         self.timer = self.create_timer(timer_period, self.publish_callback)
     
     def publish_callback(self):
-        # Read vision constant from NetworkTables
-        if (self.nt_publish == True):
-            new_vision_constant = self.vision_constant_sub.get()
-            # Log if value changed
-            if new_vision_constant != self.vision_constant:
-                self.get_logger().info(f'Vision constant updated from NetworkTables: {self.vision_constant:.6f} -> {new_vision_constant:.6f}')
-                self.vision_constant = new_vision_constant
+        # 1. Read vision constant from NetworkTables
+        new_vision_constant = self.vision_constant_sub.get()
+            
+        if new_vision_constant != self.vision_constant:
+           self.get_logger().info(f'Vision constant updated: {self.vision_constant:.6f} -> {new_vision_constant:.6f}')
+           self.vision_constant = new_vision_constant
+
+        # 2. Publish the constant to a ROS Topic
+        vision_msg = Float64()
+        vision_msg.data = float(self.vision_constant)
+        self.vision_constant_publisher.publish(vision_msg)
         
-        # Update parameter server with latest vision constant
-        self.set_parameters([rclpy.Parameter('vision_constant', self.vision_constant)])
-        
-        # Publish lifesigns
+        # 3. Publish lifesigns
         self.lifesigns_counter.data += 1
-        
-        if (self.nt_publish == True):
-           self.lifesigns_pub.set(self.lifesigns_counter.data) 
-                  
-        if (self.ros_publish == True):
-           self.publisher.publish(self.lifesigns_counter)
+        self.lifesigns_pub.set(self.lifesigns_counter.data)  
+    
     
 def main(args=None):
     rclpy.init(args=args)
